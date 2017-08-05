eab00266d9222d03d6c7c04bf9efe875675864ba
Teo Si-Yan <teosiyan@hotmail.com>
2017-08-05 22:29:44 +0800

    Add js scripts for animation

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // zoom in/zoom out animations
+  if ($(".content-block").hasClass('fadeOut')) {
+    $(".content-block").removeClass("fadeOut").addClass("fadeIn");
+  }
+  $(".navbtn").click(function() {
+    $(".content-block").removeClass("fadeIn").addClass("fadeOut");
+  });
+});
