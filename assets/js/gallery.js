4f61a51d23524797ec37f799f6be27c3b0fd4311
Teo Si-Yan <teosiyan@hotmail.com>
2017-10-30 17:22:13 +0800

    Add photos to gallery

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,24 +1,59 @@
 $(document).ready(function() {
   // fetch and display data for photog gallery
-  if($("#photoggallery").length > 0) {
+  if($("#galleries-list").length > 0) {
     _500px.init({
       sdk_key: 'c05562552532e685ce126f553f266817dd9f1664'
     });
 
     _500px.api('/users/22764323/galleries', function (response) {
       galleries = response.data.galleries;
-      displayGallery(galleries);
+      displayGalleryList(galleries);
     });
   }
 })
 
-createCard = function(image_url, title) {
-  return "<div class='relative'><img class='100px' src='"+image_url+"' /><h2 class='gallery-title'>"+title+"</h2></div>"
+galleryCard = function(gallery_id, image_src, title) {
+  return (
+    `<div class='gallery-thumb relative' id='${gallery_id}'>
+      <img class='500px' src='${image_src}' />
+      <h2 class='gallery-title'>${title}</h2>
+    </div>`
+  )
+}
+
+displayGalleryList = function(galleries) {
+  $("#galleries-list").before("<h2>Galleries (click to view)</h2>");
+  for (i=0; i<galleries.length; i++) {
+    g = galleries[i];
+    if (g.items_count == 0) continue;
+    $("#galleries-list").append(galleryCard(g.id, g.thumbnail_photos[0].image_url, g.name));
+    $('#'+g.id).click(function(e){ fetchGallery(e.currentTarget.id) });
+  }
+}
+
+fetchGallery = function(id) {
+  $(".gallery-thumb").css("opacity", 0.5);
+  $("#"+id).css("opacity", 1);
+  $("h2#photos").remove();
+  $("#gallery-photos").empty();
+  _500px.api('/users/22764323/galleries/'+id+'/items', {image_size: 4}, function (response) {
+    photos = response.data.photos;
+    displayGallery(photos);
+  });
 }
 
 displayGallery = function(photos) {
+  $("#gallery-photos").before("<h2 id='photos'>Photos</h2>");
   for (i=0; i<photos.length; i++) {
-    if (photos[i].items_count == 0) continue;
-    $("#photoggallery").append(createCard(photos[i].thumbnail_photos[0].image_url, photos[i].name));
+    p = photos[i];
+    $("#gallery-photos").append(photoCard(p.id, p.image_url, p.url));
   }
+}
+
+photoCard = function(gallery_id, image_src, image_url) {
+  return (
+    `<a href="https://500px.com${image_url}" target="_blank"><div class='relative' id='${gallery_id}'>
+      <img class='500px' src='${image_src}' />
+    </div></a>`
+  )
 }
